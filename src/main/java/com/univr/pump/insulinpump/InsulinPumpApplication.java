80bd3f6a86524a96689fabc3ebb512dd108b2587
alessiogj <alessiovr1@gmail.com>
2024-01-02 22:01:05 +0100

    first commit

---
+++ src/main/java/com/univr/pump/insulinpump/InsulinPumpApplication.java
@@ -0,0 +1,13 @@
+package com.univr.pump.insulinpump;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsulinPumpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsulinPumpApplication.class, args);
+    }
+
+}
