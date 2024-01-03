69740272809b48ac2bd42d67bf7d89f0a5df4323
alessiogj <alessiovr1@gmail.com>
2024-01-03 10:56:31 +0100

    Check dependencies and springboot version

--- src/test/java/com/univr/pump/insulinpump/InsulinPumpApplicationTests.java
+++ src/test/java/com/univr/pump/insulinpump/InsulinPumpApplicationTests.java
@@ -1,13 +1,17 @@
 package com.univr.pump.insulinpump;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class InsulinPumpApplicationTests {
 
     @Test
     void contextLoads() {
+        System.out.println("Hello World!");
     }
 
 }
