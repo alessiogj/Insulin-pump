a8a805d249555847fe593fa63b8c430bb94ad699
nickkpiccoli <piccolibeatz@gmail.com>
2024-01-08 09:21:07 +0100

    WIP: new ui

--- src/main/java/com/univr/pump/insulinpump/controller/AppController.java
+++ src/main/java/com/univr/pump/insulinpump/controller/AppController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AppController {
     @RequestMapping("/")
     public String index(){
-        return "index";
+        return "indexcopy";
     }
 
 }
