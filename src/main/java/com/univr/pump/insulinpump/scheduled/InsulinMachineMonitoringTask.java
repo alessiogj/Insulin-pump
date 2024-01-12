9326dcd207e8f86eb243045bd99e587b01d7bcc5
alessiogj <alessiovr1@gmail.com>
2024-01-12 16:04:27 +0100

    refactoring tasks

--- src/main/java/com/univr/pump/insulinpump/scheduled/InsulinMachineMonitoringTask.java
+++ src/main/java/com/univr/pump/insulinpump/scheduled/InsulinMachineMonitoringTask.java
@@ -2,6 +2,7 @@ package com.univr.pump.insulinpump.scheduled;
 
 import com.univr.pump.insulinpump.mock.Patient;
 import com.univr.pump.insulinpump.service.InsulinMachineService;
+import com.univr.pump.insulinpump.service.VitalParametersService;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -9,11 +10,14 @@ import org.springframework.stereotype.Component;
 public class InsulinMachineMonitoringTask {
     private final Patient patient;
     private final InsulinMachineService insulinMachineService;
+    private final VitalParametersService vitalParametersService;
 
     public InsulinMachineMonitoringTask(Patient patient,
-                                        InsulinMachineService insulinMachineService) {
+                                        InsulinMachineService insulinMachineService,
+                                        VitalParametersService vitalParametersService) {
         this.patient = patient;
         this.insulinMachineService = insulinMachineService;
+        this.vitalParametersService = vitalParametersService;
     }
 
     /**
@@ -41,6 +45,26 @@ public class InsulinMachineMonitoringTask {
             insulinMachineService.injectInsulin();
             patient.setGlucoseToNormal();
         }
+    }
+
+    /**
+     * Ogni 10 minuti effettua una misurazione dei parametri vitali del paziente.
+     * Se la batteria è scarica la misurazione
+     * non viene effettuata.
+     */
+    @Scheduled(fixedRate = 1000)
+    public void newVitalSigns() {
+        if(insulinMachineService.getBatteryLevel() == 0) {
+            return;
+        }
+
+        vitalParametersService.saveVitalParameters(
+                patient.getPressureSystolic(),
+                patient.getPressureDiastolic(),
+                patient.getHeartRate(),
+                patient.getGlucoseLevel(),
+                patient.getTemperature()
+        );
     }
 
 }
--- src/main/java/com/univr/pump/insulinpump/scheduled/VitalParametersMonitoringTask.java
+++ src/main/java/com/univr/pump/insulinpump/scheduled/VitalParametersMonitoringTask.java
@@ -9,16 +9,10 @@ import org.springframework.stereotype.Component;
 @Component
 public class VitalParametersMonitoringTask {
 
-    private final VitalParametersService vitalParametersService;
-    private final InsulinMachineService insulinMachineService;
 
     private final Patient patient;
 
-    public VitalParametersMonitoringTask(VitalParametersService vitalParametersService,
-                                         InsulinMachineService insulinMachineService,
-                                         Patient patient) {
-        this.vitalParametersService = vitalParametersService;
-        this.insulinMachineService = insulinMachineService;
+    public VitalParametersMonitoringTask(Patient patient) {
         this.patient = patient;
     }
 
@@ -31,29 +25,5 @@ public class VitalParametersMonitoringTask {
         patient.modifyPressure();
         patient.modifyBloodGlucose();
         patient.modifyTemperature();
-        System.out.println("Diastolic pressure: " + patient.getPressureDiastolic());
-        System.out.println("Systolic pressure: " + patient.getPressureSystolic());
-        System.out.println("Blood glucose: " + patient.getGlucoseLevel());
-        System.out.println("Temperature: " + patient.getTemperature());
-    }
-
-    /**
-     * Ogni 10 minuti effettua una misurazione dei parametri vitali del paziente.
-     * Se la batteria è scarica la misurazione
-     * non viene effettuata.
-     */
-    @Scheduled(fixedRate = 1000)
-    public void newVitalSigns() {
-        if(insulinMachineService.getBatteryLevel() == 0) {
-            return;
-        }
-
-        vitalParametersService.saveVitalParameters(
-                patient.getPressureSystolic(),
-                patient.getPressureDiastolic(),
-                patient.getHeartRate(),
-                patient.getGlucoseLevel(),
-                patient.getTemperature()
-        );
     }
 }
