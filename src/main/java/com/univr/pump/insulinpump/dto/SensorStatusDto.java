77eba0f81e4c91e311036d91a4ef233e9d4a44f8
alessiogj <alessiovr1@gmail.com>
2024-01-16 08:55:10 +0100

    Modified lombock annotation

--- src/main/java/com/univr/pump/insulinpump/dto/SensorStatusDto.java
+++ src/main/java/com/univr/pump/insulinpump/dto/SensorStatusDto.java
@@ -1,8 +1,10 @@
 package com.univr.pump.insulinpump.dto;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 public class SensorStatusDto {
     private int battery;
     private int tank;
--- src/main/java/com/univr/pump/insulinpump/dto/VitalParametersDto.java
+++ src/main/java/com/univr/pump/insulinpump/dto/VitalParametersDto.java
@@ -1,8 +1,10 @@
 package com.univr.pump.insulinpump.dto;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 public class VitalParametersDto {
     private String timestamp;
     private int bloodPressureSystolic;
